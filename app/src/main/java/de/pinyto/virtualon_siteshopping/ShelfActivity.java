037378fa1f61624f2ba24328801b5083b4f11469
Karola <yonjuni@pinyto.de>
2017-01-08 21:45:45 +0200

    Opening of browser to display shelf added

---
+++ app/src/main/java/de/pinyto/virtualon_siteshopping/ShelfActivity.java
@@ -0,0 +1,26 @@
+package de.pinyto.virtualon_siteshopping;
+
+/**
+ * Created by yonjuni on 08.01.17.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import java.io.File;
+
+public class ShelfActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Uri uri = Uri.fromFile(new File("storage/emulated/0/Download/prototype/virtualshelf.html"));
+        Intent in = new Intent(Intent.ACTION_VIEW);
+        in.setDataAndType(uri, "text/html");
+        startActivity(in);
+
+    }
+}
